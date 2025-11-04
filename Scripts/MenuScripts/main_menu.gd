e4b53f5726b690b935ec55d992656ee7313cd1d6
JohnSherman1999 <sherman.j172118@stu.sanjac.edu>
2025-11-04 12:59:43 -0600

    Settings Menu

--- Scripts/MenuScripts/main_menu.gd
+++ Scripts/MenuScripts/main_menu.gd
@@ -2,10 +2,14 @@ extends Node2D
 
 
 func _on_play_pressed() -> void:
-		get_tree().change_scene_to_file("res://aLevels/test_forest.tscn")
+	get_tree().change_scene_to_file("res://aLevels/test_forest.tscn")
 	
 
 func _on_quit_pressed() -> void:
-		get_tree().quit()
+	get_tree().quit()
 	
 	
+
+
+func _on_options_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Settings/options.tscn")
